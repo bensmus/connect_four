8fc8f4f782b20ba01710ff6d334726274143fb5b
Ben Smus <bensmus@gmail.com>
2024-08-29 12:19:31 -0700

    added comments

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
+// Grid div that holds cell divs.
 const cellContainer = document.querySelector("#cell-container")
+
+// Grid div that holds trigger buttons.
 const triggerContainer = document.querySelector("#trigger-container")
+
+// Either "red" or "yellow" innerText.
 const turnIndicator = document.querySelector("#turn-indicator")
+
+// Player alternates between 1 and -1.
 let player = 1
 
 const rowCount = 6
 const colCount = 7
 
+// Create 2D array of numnbers containing 0, 1, or -1 which
+// is current board state (empty, player 1, or player -1),
+// and add cell divs to cell container.
 function initGrid() {
     const grid = []
     for (let i = 0; i < rowCount; i++) {
@@ -23,14 +33,11 @@ function initGrid() {
 
 const grid = initGrid()
 
+// Respond to column triggered:
+// Find the position of the affected cell,
+// Update UI and grid.
 function playColumn(j) {
-    function setCell(i, j) {
-        grid[i][j] = player
-        const cell = cellContainer.children[i * colCount + j]
-        console.log(cell)
-        cell.style.backgroundColor = player == 1 ? 'red' : 'yellow'
-    }
-
+    // Func to read grid column.
     function readColumn(j) {
         const column = []
         for (let i = 0; i < rowCount; i++) {
@@ -39,15 +46,26 @@ function playColumn(j) {
         return column
     }
 
+    // Func to update UI and grid.
+    function setCell(i, j) {
+        grid[i][j] = player
+        const cell = cellContainer.children[i * colCount + j]
+        console.log(cell)
+        cell.style.backgroundColor = player == 1 ? 'red' : 'yellow'
+    }
+
+    // Scan column for empty cell for player to play:
     const column = readColumn(j)
     for (let i = rowCount - 1; i > -1; i--) {
-        if (column[i] == 0) {
-            setCell(i, j, player)
-            player *= -1
-            turnIndicator.innerText = player == 1 ? 'red' : 'yellow'
+        if (column[i] == 0) { // Empty cell found:
+            setCell(i, j, player) // Update UI and grid.
+            player *= -1 // Alternate player.
+            turnIndicator.innerText = player == 1 ? 'red' : 'yellow' // Update turn indicator text.
             return
         }
     }
+
+    // If here, empty cell not found, player not updated, no action taken.
 }
 
 for (let i = 0; i < colCount; i++) {
