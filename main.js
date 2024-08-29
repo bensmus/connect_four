ebcb23e75720305c9bd067461d1a20665cbbc303
Ben Smus <bensmus@gmail.com>
2024-08-29 12:05:18 -0700

    fixed grid

--- main.js
+++ main.js
@@ -3,9 +3,12 @@ const triggerContainer = document.querySelector("#trigger-container")
 const turnIndicator = document.querySelector("#turn-indicator")
 let player = 1
 
+const rowCount = 6
+const colCount = 7
+
 function initGrid() {
     const grid = []
-    for (let i = 0; i < 7; i++) {
+    for (let i = 0; i < rowCount; i++) {
         const row = []
         for (let j = 0; j < 7; j++) {
             const cell = document.createElement('div')
@@ -23,21 +26,21 @@ const grid = initGrid()
 function playColumn(j) {
     function setCell(i, j) {
         grid[i][j] = player
-        const cell = cellContainer.children[i * 7 + j]
+        const cell = cellContainer.children[i * colCount + j]
         console.log(cell)
         cell.style.backgroundColor = player == 1 ? 'red' : 'yellow'
     }
 
     function readColumn(j) {
         const column = []
-        for (let i = 0; i < 7; i++) {
+        for (let i = 0; i < rowCount; i++) {
             column.push(grid[i][j])
         }
         return column
     }
 
     const column = readColumn(j)
-    for (let i = 6; i > -1; i--) {
+    for (let i = rowCount - 1; i > -1; i--) {
         if (column[i] == 0) {
             setCell(i, j, player)
             player *= -1
@@ -47,7 +50,7 @@ function playColumn(j) {
     }
 }
 
-for (let i = 0; i < 7; i++) {
+for (let i = 0; i < colCount; i++) {
     const colTrigger = document.createElement('button')
     colTrigger.innerText = '↓'
     colTrigger.classList.add('col-trigger')
