be82cc5760b97f46eeec83af5ec10fcd98ad7708
Ben Smus <bensmus@gmail.com>
2024-08-30 16:20:14 -0700

    big refactor

--- main.js
+++ main.js
@@ -1,104 +1,67 @@
-// Grid div that holds cell divs.
-const cellContainer = document.querySelector("#cell-container")
-
-// Grid div that holds trigger buttons.
-const triggerContainer = document.querySelector("#trigger-container")
-
-// Either "red" or "yellow" innerText.
-const infoText = document.querySelector("#info-text")
-
-const replayButton = document.querySelector('#replay-button')
-
-// Player alternates between 1 and -1.
-let player = 1
-
 const rowCount = 6
-const colCount = 7
+const columnCount = 7
 
-// Create 2D array of numbers containing 0, 1, or -1 which
-// is current board state (empty, player 1, or player -1),
-// and add cell divs to cell container.
-const grid = (function initGrid() {
-    const grid = []
-    for (let i = 0; i < rowCount; i++) {
-        const row = []
-        for (let j = 0; j < colCount; j++) {
-            const cell = document.createElement('div')
-            cell.classList.add('cell')
-            cellContainer.appendChild(cell)
-            row.push(0)
-        }
-        grid.push(row)
-    }
-    return grid
-})()
-
-// Enable or disable triggers.
-function setTriggers(to) {
-    for (child of triggerContainer.children) {
-        child.disabled = to
+function playerToColor(player) {
+    if (player == 1) { // Player 1: red
+        return 'red'
+    } else if (player == -1) { // Player -1: yellow
+        return 'yellow'
+    } else {
+        throw new Error("Player integer must be -1 or 1");
     }
 }
 
-function resetGame() {
-    replayButton.disabled = true
-    infoText.innerText = 'red turn'
-    setTriggers(false)
-    player = 1
-    for (let i = 0; i < rowCount; i++) {
-        for (let j = 0; j < colCount; j++) {
-            grid[i][j] = 0
-            const cell = cellContainer.children[i * colCount + j]
-            cell.style.backgroundColor = 'white'
-        }
-    }
-}
-
-replayButton.addEventListener('click', resetGame)
-
-// Respond to column triggered:
-// Find the position of the affected cell,
-// Update UI and grid.
-function playColumn(j) {
-    // Func to get player color.
-    function playerColor(player) {
-        return player == 1 ? 'red' : 'yellow' 
-    }
-
-    function readLine(iStart, jStart, iDelta, jDelta, count) {
-        const line = []
-        let i = iStart
-        let j = jStart
-        for (let x = 0; x < count; x++) {
-            if (i < rowCount && i >= 0 && j < colCount && j >= 0) {
-                line.push(grid[i][j])
+class Board {
+    constructor(cellContainer) {
+        this.cellContainer = cellContainer
+        this.tokens = [] // Where are player tokens and empty spaces: 2D array.
+        for (let i = 0; i < rowCount; i++) {
+            const row = []
+            for (let j = 0; j < columnCount; j++) {
+                const cell = document.createElement('div')
+                cell.classList.add('cell')
+                cellContainer.appendChild(cell)
+                row.push(0)
             }
-            i += iDelta
-            j += jDelta
+            this.tokens.push(row)
         }
-        return line
+    }
+
+    findTokenDropRow(column) {
+        // Start from bottom:
+        for (let row = rowCount - 1; row > -1; row--) {
+            if (this.tokens[row][column] == 0) {
+                return row
+            }
+        }
+        return -1
     }
     
-    // Func to read grid column.
-    function readColumn(j) {
-        return readLine(0, j, 1, 0, rowCount)
+    setToken(row, column, player) {
+        this.tokens[row][column] = player
+        const cell = this.cellContainer.children[row * columnCount + column]
+        cell.style.backgroundColor = playerToColor(player)
     }
 
-    // Func to update UI and grid.
-    function setCell(i, j) {
-        grid[i][j] = player
-        const cell = cellContainer.children[i * colCount + j]
-        cell.style.backgroundColor = playerColor(player)
-    }
+    checkWin(rowLatest, columnLatest) {
+        function readLine(ar, rowStart, columnStart, rowDelta, columnDelta, lineLength) {
+            const line = []
+            let row = rowStart
+            let column = columnStart
+            for (let iter = 0; iter < lineLength; iter++) {
+                if (row < rowCount && row >= 0 && column < columnCount && column >= 0) {
+                    line.push(ar[row][column])
+                }
+                row += rowDelta
+                column += columnDelta
+            }
+            return line
+        }
 
-    // Func to check win, given i, j move was the last one made.
-    function checkWin(i, j) {
-        const playerToCheck = grid[i][j]
-
-        function fourInARow(ar) {
+        function fourInARow(ar, elem) {
             let countInARow = 0
             for (let i = 0; i < ar.length; i++) {
-                if (ar[i] == playerToCheck) {
+                if (ar[i] == elem) {
                     countInARow++
                     if (countInARow == 4) {
                         return true
@@ -110,49 +73,106 @@ function playColumn(j) {
             return false
         }
 
+        const playerToCheck = this.tokens[rowLatest][columnLatest]
+
         const lineLength = 7
-        const horiz = readLine(i, j - 3, 0, 1, lineLength)
-        const vert = readLine(i - 3, j, 1, 0, lineLength)
-        const diagDown = readLine(i - 3, j - 3, 1, 1, lineLength)
-        const diagUp = readLine(i + 3, j - 3, -1, 1, lineLength)
-        return fourInARow(horiz) || fourInARow(vert) || fourInARow(diagDown) || fourInARow(diagUp)
+        const horiz = readLine(this.tokens, rowLatest, columnLatest - 3, 0, 1, lineLength)
+        const vert = readLine(this.tokens, rowLatest - 3, columnLatest, 1, 0, lineLength)
+        const diagDown = readLine(this.tokens, rowLatest - 3, columnLatest - 3, 1, 1, lineLength)
+        const diagUp = readLine(this.tokens, rowLatest + 3, columnLatest - 3, -1, 1, lineLength)
+        return (
+            fourInARow(horiz, playerToCheck) || fourInARow(vert, playerToCheck) ||
+            fourInARow(diagDown, playerToCheck) || fourInARow(diagUp, playerToCheck)
+        )
     }
 
-    // Handle putting the connect four token at i, j.
-    function putToken(i, j) {
-        setCell(i, j, player) // Update cell color and grid.
-        // Winning move:
-        if (checkWin(i, j)) { // Update text and disable all triggers.
-            infoText.innerText = `${playerColor(player)} wins`
-            setTriggers(true)
-            replayButton.disabled = false
-        } 
-        // Normal move:
-        else {
-            player *= -1 // Alternate player.
-            infoText.innerText = `${playerColor(player)} turn` // Update turn indicator text.
+    reset() {
+        for (let i = 0; i < rowCount; i++) {
+            for (let j = 0; j < columnCount; j++) {
+                this.tokens[i][j] = 0
+                const cell = this.cellContainer.children[i * columnCount + j]
+                cell.style.backgroundColor = 'white'
+            }
+        }
+    }
+}
+
+class ColumnTriggers {
+    constructor(triggerContainer, callback) {
+        this.triggerContainer = triggerContainer
+        for (let column = 0; column < columnCount; column++) {
+            const columnTrigger = document.createElement('button')
+            columnTrigger.addEventListener('click', () => {
+                callback(column)
+            })
+            columnTrigger.innerText = '↓'
+            columnTrigger.classList.add('col-trigger')
+            triggerContainer.appendChild(columnTrigger)
         }
     }
 
-    // Scan column for empty cell for player to play:
-    const column = readColumn(j)
-
-    for (let i = rowCount - 1; i > -1; i--) {
-        if (column[i] == 0) { // Empty row found for that column, i.e. empty cell found:
-            putToken(i, j)
-            break
+    enable() {
+        for (const child of this.triggerContainer.children) {
+            child.disabled = false
         }
     }
 
-    // If here, empty cell not found, player not updated, no action taken.
+    disable() {
+        for (const child of this.triggerContainer.children) {
+            child.disabled = true
+        }
+    }
 }
 
-for (let i = 0; i < colCount; i++) {
-    const colTrigger = document.createElement('button')
-    colTrigger.innerText = '↓'
-    colTrigger.classList.add('col-trigger')
-    colTrigger.addEventListener('click', () => {
-        playColumn(i)
-    })
-    triggerContainer.appendChild(colTrigger)
+class ReplayButton {
+    constructor(replayButton, callback) {
+        this.replayButton = replayButton
+        this.replayButton.addEventListener('click', callback)
+    }
+
+    enable() {
+        this.replayButton.disabled = false
+    }
+
+    disable() {
+        this.replayButton.disabled = true
+    }
 }
+
+// Grid div that holds cell divs.
+const board = new Board(document.querySelector("#cell-container"))
+
+// E.g. "red turn", "yellow wins".
+const infoText = document.querySelector("#info-text")
+
+let player = 1
+
+function dropToken(column) {
+    const row = board.findTokenDropRow(column)
+    if (row == -1) {
+        return  
+    } 
+    board.setToken(row, column, player)
+    if (board.checkWin(row, column)) {
+        infoText.innerText = `${playerToColor(player)} wins`
+        columnTriggers.disable()
+        replayButton.enable()
+    } else {
+        player *= -1
+        infoText.innerText = `${playerToColor(player)} turn`
+    }
+}
+
+function replay() {
+    board.reset()
+    columnTriggers.enable()
+    replayButton.disable()
+    player = 1
+    infoText.innerText = 'red turn'
+}
+
+// Resets game state.
+const replayButton = new ReplayButton(document.querySelector('#replay-button'), replay)
+
+// Grid div that holds column triggers.
+const columnTriggers = new ColumnTriggers(document.querySelector("#trigger-container"), dropToken)
